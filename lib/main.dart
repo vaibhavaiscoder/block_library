22cc1679a13a6b710feaeb3883e06f77d8cdd775
vaibhavaiscoder <vaibhavghugase00@gmail.com>
2023-09-27 18:46:44 +0530

    shared preferences ex

--- lib/main.dart
+++ lib/main.dart
@@ -1,191 +1,139 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/board_tile.dart';
-import 'package:flutter_application_1/tile_state.dart';
+import 'package:flutter_application_1/models.dart';
+import 'package:flutter_application_1/preferences_service.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   State<StatefulWidget> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  final navigatorKey = GlobalKey<NavigatorState>();
-  var _boardState = List.filled(9, TileState.EMPTY);
+  final _preferencesService = PreferencesService();
 
-  var _currentTurn = TileState.CROSS;
+  final _usernameController = TextEditingController();
+  var _selectedGender = Gender.FEMALE;
+  var _selectedLanguages = <ProgrammingLanguage>{};
+  var _isEmployed = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _populateFields();
+  }
+
+  void _populateFields() async {
+    final settings = await _preferencesService.getSettings();
+    setState(() {
+      _usernameController.text = settings.username;
+      _selectedGender = settings.gender;
+      _selectedLanguages = settings.programmingLanguages;
+      _isEmployed = settings.isEmployed;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      navigatorKey: navigatorKey,
       home: Scaffold(
-        body: Center(
-          child: Stack(
-            children: [
-              Image.asset('assets/board.png'),
-              _boardTiles(),
-            ],
-          ),
+        appBar: AppBar(
+          title: const Text('User Settings'),
+        ),
+        body: ListView(
+          children: [
+            ListTile(
+              title: TextField(
+                controller: _usernameController,
+                decoration: const InputDecoration(labelText: 'Username'),
+              ),
+            ),
+            RadioListTile(
+                title: const Text('Female'),
+                value: Gender.FEMALE,
+                groupValue: _selectedGender,
+                onChanged: (newValue) =>
+                    setState(() => _selectedGender = newValue!)),
+            RadioListTile(
+                title: const Text('Male'),
+                value: Gender.MALE,
+                groupValue: _selectedGender,
+                onChanged: (newValue) =>
+                    setState(() => _selectedGender = newValue!)),
+            RadioListTile(
+                title: const Text('Other'),
+                value: Gender.OTHER,
+                groupValue: _selectedGender,
+                onChanged: (newValue) =>
+                    setState(() => _selectedGender = newValue!)),
+            CheckboxListTile(
+                title: const Text('Dart'),
+                value: _selectedLanguages.contains(ProgrammingLanguage.DART),
+                onChanged: (_) {
+                  setState(() {
+                    _selectedLanguages.contains(ProgrammingLanguage.DART)
+                        ? _selectedLanguages.remove(ProgrammingLanguage.DART)
+                        : _selectedLanguages.add(ProgrammingLanguage.DART);
+                  });
+                }),
+            CheckboxListTile(
+                title: const Text('JavaScript'),
+                value:
+                    _selectedLanguages.contains(ProgrammingLanguage.JAVASCRIPT),
+                onChanged: (_) {
+                  setState(() {
+                    _selectedLanguages.contains(ProgrammingLanguage.JAVASCRIPT)
+                        ? _selectedLanguages
+                            .remove(ProgrammingLanguage.JAVASCRIPT)
+                        : _selectedLanguages
+                            .add(ProgrammingLanguage.JAVASCRIPT);
+                  });
+                }),
+            CheckboxListTile(
+                title: const Text('Kotlin'),
+                value: _selectedLanguages.contains(ProgrammingLanguage.KOTLIN),
+                onChanged: (_) {
+                  setState(() {
+                    _selectedLanguages.contains(ProgrammingLanguage.KOTLIN)
+                        ? _selectedLanguages.remove(ProgrammingLanguage.KOTLIN)
+                        : _selectedLanguages.add(ProgrammingLanguage.KOTLIN);
+                  });
+                }),
+            CheckboxListTile(
+                title: const Text('Swift'),
+                value: _selectedLanguages.contains(ProgrammingLanguage.SWIFT),
+                onChanged: (_) {
+                  setState(() {
+                    _selectedLanguages.contains(ProgrammingLanguage.SWIFT)
+                        ? _selectedLanguages.remove(ProgrammingLanguage.SWIFT)
+                        : _selectedLanguages.add(ProgrammingLanguage.SWIFT);
+                  });
+                }),
+            SwitchListTile(
+                title: const Text('Is Employed'),
+                value: _isEmployed,
+                onChanged: (newValue) =>
+                    setState(() => _isEmployed = newValue)),
+            TextButton(
+                onPressed: _saveSettings, child: const Text('Save Settings'))
+          ],
         ),
       ),
     );
   }
 
-  Widget _boardTiles() {
-    return Builder(builder: (context) {
-      final boardDimension = MediaQuery.of(context).size.width;
-      final tileDimension = boardDimension / 3;
+  void _saveSettings() {
+    final newSettings = Settings(
+        username: _usernameController.text,
+        gender: _selectedGender,
+        programmingLanguages: _selectedLanguages,
+        isEmployed: _isEmployed);
 
-      return SizedBox(
-        width: boardDimension,
-        height: boardDimension,
-        child: Column(
-          children: chunk(_boardState, 3).asMap().entries.map((entry) {
-            final chunkIndex = entry.key;
-            final tileStateChunk = entry.value;
-
-            return Row(
-              children: tileStateChunk.asMap().entries.map((innerEntry) {
-                final innerIndex = innerEntry.key;
-                final tileState = innerEntry.value;
-                final tileIndex = (chunkIndex * 3) + innerIndex;
-
-                return BoardTile(
-                  tileState: tileState,
-                  dimension: tileDimension,
-                  onPressed: () => _updateTileStateForIndex(tileIndex),
-                );
-              }).toList(),
-            );
-          }).toList(),
-        ),
-      );
-    });
+    print(newSettings);
+    _preferencesService.saveSettings(newSettings);
   }
-
-  void _updateTileStateForIndex(int selectedIndex) {
-    if (_boardState[selectedIndex] == TileState.EMPTY) {
-      setState(() {
-        _boardState[selectedIndex] = _currentTurn;
-        _currentTurn = _currentTurn == TileState.CROSS
-            ? TileState.CIRCLE
-            : TileState.CROSS;
-      });
-
-      final winner = _findWinner();
-      if (winner != null) {
-        print('Winner is: $winner');
-        _showWinnerDialog(winner);
-      } else if (!_boardState.contains(TileState.EMPTY)) {
-        // Check for a draw (no empty tiles left)
-        print('It\'s a draw!');
-        _showDrawDialog();
-      }
-    }
-  }
-
-  void _showDrawDialog() {
-    final context = navigatorKey.currentState!.overlay!.context;
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (_) {
-        return AlertDialog(
-          title: const Text('Draw'),
-          content: const Text('The game ended in a draw.'),
-          actions: [
-            TextButton(
-              onPressed: () {
-                _resetGame();
-                Navigator.of(context).pop();
-              },
-              child: const Text('New Game'),
-            )
-          ],
-        );
-      },
-    );
-  }
-
-  TileState? _findWinner() {
-    TileState? winner;
-
-    winnerForMatch(a, b, c) {
-      if (_boardState[a] != TileState.EMPTY) {
-        if ((_boardState[a] == _boardState[b]) &&
-            (_boardState[b] == _boardState[c])) {
-          return _boardState[a];
-        }
-      }
-      return null;
-    }
-
-    final checks = [
-      winnerForMatch(0, 1, 2),
-      winnerForMatch(3, 4, 5),
-      winnerForMatch(6, 7, 8),
-      winnerForMatch(0, 3, 6),
-      winnerForMatch(1, 4, 7),
-      winnerForMatch(2, 5, 8),
-      winnerForMatch(0, 4, 8),
-      winnerForMatch(2, 4, 6),
-    ];
-
-    for (int i = 0; i < checks.length; i++) {
-      final checkResult = checks[i];
-      if (checkResult != null) {
-        winner = checkResult;
-        break;
-      }
-    }
-
-    return winner;
-  }
-
-  void _showWinnerDialog(TileState tileState) {
-    final context = navigatorKey.currentState!.overlay!.context;
-    showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (_) {
-        return AlertDialog(
-          title: const Text('Winner'),
-          content: Image.asset(
-            tileState == TileState.CROSS ? 'assets/x.png' : 'assets/o.png',
-            width: 100.0, // Adjust the width as needed
-            height: 100.0, // Adjust the height as needed
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                _resetGame();
-                Navigator.of(context).pop();
-              },
-              child: const Text('New Game'),
-            )
-          ],
-        );
-      },
-    );
-  }
-
-  void _resetGame() {
-    setState(() {
-      _boardState = List.filled(9, TileState.EMPTY);
-      _currentTurn = TileState.CROSS;
-    });
-  }
-}
-
-List<List<T>> chunk<T>(List<T> list, int chunkSize) {
-  List<List<T>> chunks = [];
-  for (int i = 0; i < list.length; i += chunkSize) {
-    chunks.add(list.sublist(i, i + chunkSize));
-  }
-  return chunks;
 }
