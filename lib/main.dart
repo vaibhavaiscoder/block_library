bbe391fd777952f2fec056d23ca7a275c5d7cd9a
vaibhavaiscoder <vaibhavghugase00@gmail.com>
2023-09-27 17:06:13 +0530

    webview in flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,60 @@
-import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/flash_card.dart';
-import 'package:flutter_application_1/flash_card_view.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<StatefulWidget> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyWebView(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  final List<Flashcard> _flashcards = [
-    Flashcard(
-        question: "What programming language does Flutter use?",
-        answer: "Dart"),
-    Flashcard(question: "Who you gonna call?", answer: "Ghostbusters!"),
-    Flashcard(question: "Who are you?", answer: "chutya!")
-  ];
+class MyWebView extends StatefulWidget {
+  const MyWebView({super.key});
 
-  int _currentIndex = 0;
+  @override
+  _MyWebViewState createState() => _MyWebViewState();
+}
+
+class _MyWebViewState extends State<MyWebView> {
+  late InAppWebViewController webView;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                  width: 250,
-                  height: 250,
-                  child: FlipCard(
-                      front: FlashcardView(
-                        text: _flashcards[_currentIndex].question,
-                      ),
-                      back: FlashcardView(
-                        text: _flashcards[_currentIndex].answer,
-                      ))),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  TextButton.icon(
-                    onPressed: showPreviousCard,
-                    icon: const Icon(Icons.chevron_left),
-                    label: const Text('Prev'),
-                  ),
-                  TextButton.icon(
-                    onPressed: showNextCard,
-                    icon: const Icon(Icons.chevron_right),
-                    label: const Text('Next'),
-                  )
-                ],
-              )
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              'WEBVIEW',
+              style: TextStyle(color: Colors.blue),
+            ),
+            Text(
+              '🌐',
+              style: TextStyle(color: Colors.blue),
+            ),
+          ],
         ),
       ),
+      body: InAppWebView(
+        initialUrlRequest: URLRequest(
+            url: Uri.parse(
+                'https://youtube.com')), // Replace with your desired URL
+        onWebViewCreated: (controller) {
+          webView = controller;
+        },
+      ),
     );
-  }
-
-  void showNextCard() {
-    setState(() {
-      _currentIndex =
-          (_currentIndex + 1 < _flashcards.length) ? _currentIndex + 1 : 0;
-    });
-  }
-
-  void showPreviousCard() {
-    setState(() {
-      _currentIndex =
-          (_currentIndex - 1 >= 0) ? _currentIndex - 1 : _flashcards.length - 1;
-    });
   }
 }
